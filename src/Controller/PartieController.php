a25f3bb00cd7f2b3c4a13cfa9c2a768ca55519c4
DBcide <olivier.francais.stage@gmail.com>
2025-05-13 09:48:12 +0200

    modify partie controller

--- src/Controller/PartieController.php
+++ src/Controller/PartieController.php
@@ -10,19 +10,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/partie')]
+#[IsGranted('ROLE_USER')] // Permet de restreindre le controller uniquement aux utilisateurs authentifier
 final class PartieController extends AbstractController
 {
     #[Route(name: 'app_partie_index', methods: ['GET'])]
     public function index(PartieRepository $partieRepository): Response
     {
-        $user = $this->getUser();
-
-        $parties = $partieRepository->findBy(['user' => $user]);
-
         return $this->render('partie/index.html.twig', [
-            'parties' => $partieRepository->findAll(),
+            'parties' => $partieRepository->findBy(['user' => $this->getUser()]), // $this->getUser() : permet d'obtenir l'utilisateur connecté
         ]);
     }
 
--- src/Controller/TopScoresController.php
+++ src/Controller/TopScoresController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Jeu;
+use App\Entity\Partie;
 use App\Form\ListeJeuxForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +14,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class TopScoresController extends AbstractController
 {
     #[Route('/topscores/{id}', name: 'app_top_scores')]
-    public function index(Request $request, Jeu $jeu): Response
+    public function index(Request $request, $id, EntityManagerInterface $entityManager): Response
     {
+        $jeu = $entityManager->getRepository(Jeu::class)->find($id);
+
         $formListJeux = $this->createForm(ListeJeuxForm::class);
         $formListJeux->handleRequest($request);
 
         if ($formListJeux->isSubmitted() && $formListJeux->isValid()) {
             $jeu = $formListJeux->get('jeux')->getData();
+
+            return $this->redirectToRoute('app_top_scores', ['id' => $jeu->getId()]);
         }
+
+        $scores = $entityManager->getRepository(Partie::class)->findBy(['jeu' => $jeu]);
+        dd($scores);
 
         return $this->render('top_scores/index.html.twig', [
             'formListJeux' => $formListJeux,
