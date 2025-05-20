6311a5f9fa6de717f235354f5e953f45902c00d2
DBcide <olivier.francais.stage@gmail.com>
2025-05-20 09:43:55 +0200

    update podium

--- src/Controller/TopScoresController.php
+++ src/Controller/TopScoresController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Jeu;
 use App\Entity\Partie;
 use App\Form\ListeJeuxForm;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,15 +24,36 @@ final class TopScoresController extends AbstractController
 
         if ($formListJeux->isSubmitted() && $formListJeux->isValid()) {
             $jeu = $formListJeux->get('jeux')->getData();
-
             return $this->redirectToRoute('app_top_scores', ['id' => $jeu->getId()]);
         }
 
-        $scores = $entityManager->getRepository(Partie::class)->findBy(['jeu' => $jeu]);
-        dd($scores);
+        $scores = $entityManager->getRepository(Partie::class)->findScoresByGameCurrentMonth(['jeu' => $jeu]);
+        
+        $first = null;
+        $second = null;
+        $third = null;
+
+        $i = 0;
+        foreach ($scores as $key => $score) {
+            if ($i == 0) {
+                $first = $score;
+            }
+            if ($i == 1) {
+                $second = $score;
+            }
+            if ($i == 2) {
+                $third = $score;
+            }
+            $i++;
+        }
 
         return $this->render('top_scores/index.html.twig', [
             'formListJeux' => $formListJeux,
+            'scores' => $scores,
+            'first' => $first,
+            'second' => $second,
+            'third' => $third,
+            'jeu' => $jeu,
         ]);
     }
 }
